87c0e63af51dc600ef4cea52cd650681f40ca7db
Deepanjan <deepanjanchat@gmail.com>
2020-08-04 12:07:29 +0530

    Daily Commit

--- CardBinCheck.c
+++ CardBinCheck.c
@@ -1,59 +1,31 @@
 /*
  * CardBinCheck.c
- *
+ * When a number is entered, tell whether this is a valid card number. Also identify the card brand.
+ * This program implements Luhn's checksum algo as a function
+ * This program implements functions to get length of a num and any digit of a given number
+ * The card brand identification is based on rules provided in CS50. They can be enhanced if needed.
  *  Created on: 04-Aug-2020
  *      Author: dc
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int getLength(long num);
+int getDigit(long num, int n);
+int luhnAlgo(long num);
 int main(void)
 {
     long num, prodSum = 0, sum = 0;
     int len, firDig, secDig, value;
     do
     {
-        long tempNum = 0;
-        len = 0;
-        num = get_long("Number:");
-        tempNum = num;
-        while (tempNum > 0)
-        {
-            ++len;
-            tempNum /= 10;
-        } //this loop gives the lenth of the number entered
-    }
-    while (num < 0); //wont accept less than 13 digits or more than 16 digits or negative numbers
-    //define a function to identify first digit and second digit based on length and entered number
-    long x = (long)(pow(10, (double)(len - 1))); //pow accepts only double, and gives result in double
-    long y = (long)(pow(10, (double)(len - 2)));
-    firDig = (int)(num / x); //calculating the first digit of the card
-    secDig = (int)((num / y) % 10); // calculating the second digit of the card
-    //printf("%ld, %d,%d, %d\n", num, len, firDig, secDig); //test to see whether the right numbers have been calculated
-    //Add a function to run Luhn's checksum
-    //Loop to identify the numbers that should be multiplied by 2
-    for (double i = 1; i < len; i += 2)
-    {
-        long a = 0, b = 0, tempNum = num;
-        a = tempNum / ((long) pow(10, i));
-        b = (a % 10) * 2;
-        if (b > 9)
-        {
-            b = b - 9;
-        }
-        prodSum += b;
-    }
-    //printf("%ld\n", prodSum); //test to see whether the right numbers have been calculated
-    //Loop to identify the numbers that should'nt be multiplied by 2
-    for (double i = 0; i < len; i += 2)
-    {
-        long a = 0, b = 0, tempNum = num;
-        a = tempNum / ((long) pow(10, i));
-        b = a % 10;
-        sum += b;
-    }
-    //printf("%ld\n", sum); //test to see whether the right numbers have been calculated
-    value = ((int)(prodSum + sum)) % 10;  //checksum
+        printf("Enter a card number:\t");
+        scanf(" %ld", &num);
+    } while (num < 0); //wont accept negative numbers
+    len = getLength(num); //calls the function to get length
+    firDig = getDigit(num, 1);
+    secDig = getDigit(num, 2);
+    value = luhnAlgo(num);
     //loop to identify card type
     if (value != 0)
     {
@@ -79,4 +51,48 @@ int main(void)
         }
     }
 }
+
+int getLength(long num){
+	int len = 0;
+	while(num > 0){
+		++len;
+	    num /= 10;
+	}
+	return len;
+}
+
+int getDigit(long num, int n){
+	int len, value = 0;
+	len = getLength(num);
+	long a = 0, b = 0;
+	a = (long)pow(10, (double)(len - n));
+	b = num / a;
+	value = b % 10;
+	return value;
+}
+
+int luhnAlgo(long num){
+	int prodSum = 0, value = 0, sum = 0;
+	int len = getLength(num);
+	for (double i = 1; i < len; i += 2){
+		long a = 0, b = 0, tempNum = num;
+		a = tempNum / ((long) pow(10, i));
+		b = (a % 10) * 2;
+	    if (b > 9){
+	    	b = b - 9;
+	    }
+	    prodSum += b;
+	}
+	for (double i = 0; i < len; i += 2){
+		long a = 0, b = 0, tempNum = num;
+		a = tempNum / ((long) pow(10, i));
+	    b = a % 10;
+	    sum += b;
+	    }
+	value = (prodSum + sum) % 10;
+	return value;
+}
+
+
+
 
